6817cc8a2e6bf15fae4342b6e17ec25d5288dd06
tcdw <admin@tcdw.net>
2017-10-11 17:24:13 +0800

    命名调整

--- bin-bmp.js
+++ bin-bmp.js
@@ -6,11 +6,7 @@ const path = require("path");
 const fs = require("fs");
 
 showHelp = function () {
-    console.log(fs.readFileSync(path.resolve(__dirname, 'docs/pixel-hider-help'), {encoding: 'utf8'}));
-}
-
-showCredit = function () {
-    console.log(fs.readFileSync(path.resolve(__dirname, 'docs/pixel-hider-credit'), {encoding: 'utf8'}));
+    console.log(fs.readFileSync(path.resolve(__dirname, 'docs/bin2bmp-help'), {encoding: 'utf8'}));
 }
 
 if (typeof argv._[0] == "undefined") {
@@ -21,24 +17,20 @@ if (typeof argv._[0] == "undefined") {
 
 switch (argv._[0]) {
     case "write":
-        require("./lib/pixel-hider/write")(
+        require("./lib/bin-bmp/write")(
             argv._[1],
-            argv._[2],
             argv.bare || false,
             argv.stdout || false
         );
         break;
     case "read":
-        require("./lib/pixel-hider/read")(
+        require("./lib/bin-bmp/read")(
             argv._[1],
             argv.stdout || false
         );
         break;
     case "help":
         showHelp();
-        break;
-    case "credit":
-        showCredit();
         break;
     case "version":
         console.log(version);
