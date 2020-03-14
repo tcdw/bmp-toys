3fad92c7047f1075ee47a759bdc830a13218fbbf
tcdw <tcdw2011@gmail.com>
2020-03-14 21:15:59 +0800

    try to update

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "indent": ["error", 4, {
+      "SwitchCase": 1
+    }],
+    "prefer-destructuring": "off",
+    "no-console": "off",
+  },
+};
